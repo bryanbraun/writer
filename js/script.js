706791dea934f1a9d961f27268d863e050a3493e
Bryan Braun <bbraun7@gmail.com>
2013-01-17 23:52:35 -0500

    Improved mobile version of site and made fixes for those with javascript disabled. Cleaned up code to meet Drupal coding standards.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 /**
- * Define a function for the mobile collapse menu.
+ * Run Javascript for the mobile collapse menu.
  */
 jQuery(document).ready(function($){
+  // Add the .js class if JS is enabled. CSS rules can use this. 
+  $('body').addClass('js');
+
   // Add in the arrow element.
   $('#main-nav > h2').append('<span id="menu-arrow"></span>');
 
@@ -9,5 +12,12 @@ jQuery(document).ready(function($){
   $('#main-nav > h2').click(function() {
     $('#menu-arrow').toggleClass('menu-open');
     $('#main-menu').slideToggle('fast');
+  });
+
+  // Solve the disappearing menu bug.
+  $(window).resize(function() {
+    if ($(window).width() <= 580) {
+      $('#main-menu').attr('style','');
+    };
   });
 });
